2b020de5aba805d1a4d7aae14950d1be6cdd291c
Jai Gobind Pal Singh <jai.gobind.pal@gmail.com>
2025-08-09 18:39:19 +0530

    created the frontend folder structure and configured the required packages for frontend

---
+++ Frontend/src/redux/slices/studentSlice.js
@@ -0,0 +1,45 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+     studentInfo:{
+          name: '',
+          rollNo: '',
+          email: '',
+          profilePicture: '',
+          phone: '',
+          class: '',
+          classSemester: '',
+     },
+     resources:{
+          groupMessages:[],
+          studyMaterials:[],
+          assignments:[],
+          announcements:[],
+     }
+}
+
+export const counterSlice = createSlice({
+     name: 'counter',
+     initialState,
+     reducers: {
+          // Define your reducers here that mutate the state
+          increment: (state) => {
+               // Redux Toolkit allows us to write "mutating" logic in reducers. It
+               // doesn't actually mutate the state because it uses the Immer library,
+               // which detects changes to a "draft state" and produces a brand new
+               // immutable state based off those changes
+               state.value += 1
+          },
+          decrement: (state) => {
+               state.value -= 1
+          },
+          incrementByAmount: (state, action) => {
+               state.value += action.payload
+          },
+     },
+})
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementByAmount } = counterSlice.actions
+
+export default counterSlice.reducer
